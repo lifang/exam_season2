8746a016574d62e0776f13925538a73509abe8fd
lifang <santhe_2004@126.com>
2011-11-29 14:41:13 +0800

    update detial

--- app/models/action_log.rb
+++ app/models/action_log.rb
@@ -2,6 +2,9 @@
 class ActionLog < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+
+  TYPES = {:LOGIN => 0, :PRACTICE => 1, :EXAM => 2, :RECITE => 3}
+  #动作类型： 0 登录  1 真题  2 模考  3 背单词
 end
 
 
