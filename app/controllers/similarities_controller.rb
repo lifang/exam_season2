50aaa0f66eaae9fa112d09753e637afb215ab16c
lifang <santhe_2004@126.com>
2011-11-30 15:32:59 +0800

    update similarities

--- app/controllers/similarities_controller.rb
+++ app/controllers/similarities_controller.rb
@@ -4,8 +4,8 @@ class SimilaritiesController < ApplicationController
   def index
     category_id = params[:category].to_i
     @similarities = Examination.paginate_by_sql(["select e.id, e.title, e.is_free from examinations e
-        where e.category_id = ? and e.status = ?", category_id, Examination::TYPES[:OLD_EXAM]],
-      :per_page => 10, :page => params[:page])
+        where e.category_id = ? and e.types = ?", category_id, Examination::TYPES[:OLD_EXAM]],
+      :per_page => 5, :page => params[:page])
     @exam_papers = {}
     examination_ids = []
     @similarities.collect { |s|  examination_ids << s.id }
@@ -13,9 +13,9 @@ class SimilaritiesController < ApplicationController
         left join papers p on epr.paper_id = p.id where epr.examination_id in (?)", examination_ids])
     papers.each do |paper|
       if @exam_papers[paper.examination_id].nil?
-        @exam_papers[paper.examination_id] << paper
+        @exam_papers[paper.examination_id] = [paper.p_title]
       else
-        @exam_papers[paper.examination_id] = [paper]
+        @exam_papers[paper.examination_id] << paper
       end
     end unless papers.blank?
   end
