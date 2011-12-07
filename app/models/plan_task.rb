dbd86b83dfd602d8497e70aea3d73fa7e77c7474
qianjun <er6788@126.com>
2011-12-07 15:07:23 +0800

    qianjun

--- app/models/plan_task.rb
+++ app/models/plan_task.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 class PlanTask < ActiveRecord::Base
   belongs_to :study_plan
-  TYPES = {:EXERCISE => 0, :READ=> 1, :SIMULATION => 2}
-  PERIOD_TYPES={:EVERY_DAY=>0,:TOTAL_PERIOD=>1}
+
 end
