e6a0d749f3d154e3bf8acde0a4ae114fdb543c22
lifang <santhe_2004@126.com>
2011-12-12 18:03:21 +0800

    update word

--- app/models/discriminate.rb
+++ app/models/discriminate.rb
@@ -1,5 +1,5 @@
 #encoding: utf-8
 class Discriminate < ActiveRecord::Base
   has_many :word_discriminate_relations,:dependent => :destroy
-  has_many :word,:through=>:word_discriminate_relations, :source => :word
+  has_many :words,:through=>:word_discriminate_relations, :source => :word
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,14 +1,14 @@
 #encoding: utf-8
 class Word < ActiveRecord::Base
   belongs_to :category
-#  has_many :word_sentences
-#  has_many :word_discriminate_relations,:dependent => :destroy
-#  has_many :discriminates,:through=>:word_discriminate_relations, :source => :discriminate
+  has_many :word_sentences
+  has_many :word_discriminate_relations,:dependent => :destroy
+  has_many :discriminates,:through=>:word_discriminate_relations, :source => :discriminate
 
-  TYPES = {"n." => 0, "v." => 1, "pron." => 2, "adj." => 3, "adv." => 4,
-    "num." => 5, "art." => 6, "prep." => 7, "conj." => 8, "interj." => 9, "u = " => 10, "c = " => 11, "pl = " => 12}
+  TYPES = {0 => "n.", 1 => "v.", 2 => "pron.", 3 => "adj.", 4 => "adv.",
+    5 => "num.", 6 => "art.", 7 => "prep.", 8 => "conj.", 9 => "interj.", 10 => "u = ", 11 => "c = ", 12 => "pl = "}
   #英语单词词性 名词 动词 代词 形容词 副词 数词 冠词 介词 连词 感叹词 不可数名词 可数名词 复数
- 
+  
   def self.get_words(category_id, search_text, page)
     sql = "select w.id, w.name from words w where w.category_id = ? "
     sql += " and name like ? " unless search_text.nil? or search_text.strip.empty?
