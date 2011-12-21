f58ea33dad39c3eee1acab3b2e87fe4d1c2ce99e
xuqiyong <jeffrey6052@163.com>
2011-12-21 12:29:04 +0800

    jeffrey

---
+++ app/assets/javascripts/kindeditor/plugins/commit1/commit.js
@@ -0,0 +1,12 @@
+KindEditor.plugin('commit', function(K) {
+    var editor = this, name = 'commit';
+    editor.clickToolbar(name, function() {
+            var text = editor.html();
+//            if($("#show_problem_title_"+block_index+"_"+problem_index).html()!=text){
+//                ajax_edit_problem_title(paper_id,block_index,problem_index,text);
+//            }
+//            active_ajax_edit_prolem_title();
+//            editor.remove();
+           alert();
+    });
+});
